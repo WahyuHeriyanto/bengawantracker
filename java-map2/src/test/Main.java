df42b70155df137135d92363908ffb705dc74aa5
WahyuHeriyanto <110623892+WahyuHeriyanto@users.noreply.github.com>
2022-11-14 22:58:30 +0700

    Update Main.java

--- java-map2/src/test/Main.java
+++ java-map2/src/test/Main.java
@@ -75,126 +75,12 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
-        label2.setText("Status :");
-
-        textField1.setText("Disconnected");
-
-        javax.swing.GroupLayout jXMapViewerLayout = new javax.swing.GroupLayout(jXMapViewer);
-        jXMapViewer.setLayout(jXMapViewerLayout);
-        jXMapViewerLayout.setHorizontalGroup(
-            jXMapViewerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jXMapViewerLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(label2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(textField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 559, Short.MAX_VALUE)
-                .addComponent(comboMapType, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-        );
-        jXMapViewerLayout.setVerticalGroup(
-            jXMapViewerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jXMapViewerLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jXMapViewerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(textField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(label2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(comboMapType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(518, Short.MAX_VALUE))
-        );
-
-        label2.getAccessibleContext().setAccessibleName("label");
-
-        getContentPane().add(jXMapViewer, new org.netbeans.lib.awtextra.AbsoluteConstraints(375, 97, 850, 550));
-
-        cmdAdd.setText("Add Waypoint");
-        cmdAdd.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cmdAddActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cmdAdd, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 510, -1, -1));
-
-        cmdClear.setText("Clear Waypoint");
-        cmdClear.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cmdClearActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cmdClear, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 510, -1, -1));
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "PORT 1", "PORT 2", "PORT 3", "PORT 4" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1010, 50, 100, -1));
-
-        jTextField1.setText("110.856315");
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 450, 255, -1));
-
-        jTextField2.setText("-7.560306");
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 380, 255, -1));
-
-        Latitude.setAlignment(java.awt.Label.CENTER);
-        Latitude.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        Latitude.setName("Latitude"); // NOI18N
-        Latitude.setText("Latitude");
-        getContentPane().add(Latitude, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 350, 50, -1));
-
-        label1.setAlignment(java.awt.Label.CENTER);
-        label1.setText("Longitude");
-        getContentPane().add(label1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 420, 60, -1));
-
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "57600", "BAUD 2", "BAUD 3", "BAUD 4" }));
-        getContentPane().add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1130, 50, 89, -1));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/buv.png"))); // NOI18N
-        jLabel1.setText("jLabel1");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1240, 670));
-
-        pack();
-        setLocationRelativeTo(null);
+        
     }// </editor-fold>//GEN-END:initComponents
 
-    private void comboMapTypeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboMapTypeActionPerformed
-        TileFactoryInfo info;
-        int index = comboMapType.getSelectedIndex();
-        if (index == 0) {
-            info = new OSMTileFactoryInfo();
-        } else if (index == 1) {
-            info = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.MAP);
-        } else if (index == 2) {
-            info = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.HYBRID);
-        } else {
-            info = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.SATELLITE);
-        }
-        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
-        jXMapViewer.setTileFactory(tileFactory);
-    }//GEN-LAST:event_comboMapTypeActionPerformed
 
-    private void cmdAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdAddActionPerformed
-       
-   
-    }//GEN-LAST:event_cmdAddActionPerformed
 
-    private void cmdClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdClearActionPerformed
-        clearWaypoint();
-    }//GEN-LAST:event_cmdClearActionPerformed
 
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox1ActionPerformed
-
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
 
     /**
      * @param args the command line arguments
